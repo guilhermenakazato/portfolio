52026e3c76c2619c06976b143505e91c05d126e5
guilhermenakazato <guilherme.fernandes23903@gmail.com>
2025-01-05 17:29:33 -0400

    [FEAT] Working on animations

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
+var body = document.querySelector("body")
+
 var loadingDiv = document.getElementById("loading")
 var loadingOne = document.getElementById("loading-one")
 var loadingTwo = document.getElementById("loading-two")
 var loadingText = document.getElementById("loading-text")
 
-setTimeout(() => {
-  loadingOne.style.width = 0
-  loadingTwo.style.width = 0
-  loadingText.style.opacity = 0
-}, 3000) 
+if (typeof(Storage) !== "undefined") {
+  // Code for localStorage/sessionStorage.
+} else {
+  // Sorry! No Web Storage support..
+}
 
-setTimeout(() => {
-  loadingDiv.remove()
-}, 4250); 
+function adjustDOM(timeout) {
+  setTimeout(() => {
+    loadingDiv.remove()
+    body.classList.remove("hide-overflow")
+  }, timeout);
+}
+
+function loadingAnimation() {
+  let adjustDOMTimeout = 0;
+  
+  if(!localStorage.getItem("loaded") == "true" || localStorage.getItem("loaded") == null) {
+    adjustDOMTimeout = 4250
+    console.log("Hi!")
+    setTimeout(() => {
+      loadingOne.style.width = 0
+      loadingTwo.style.width = 0
+      loadingText.style.opacity = 0
+    }, 3000);
+    
+    localStorage.setItem("loaded", "true") 
+  }
+  
+  adjustDOM(adjustDOMTimeout)
+}
+
+loadingAnimation();
