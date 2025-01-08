f4251a2892fe50407e91e732c0e3e875a26bcb74
guilhermenakazato <guilherme.fernandes23903@gmail.com>
2025-01-08 15:51:09 -0400

    [FEAT] Working on animations

--- scripts/loading.js
+++ scripts/loading.js
@@ -5,6 +5,11 @@ var loadingOne = document.getElementById("loading-one")
 var loadingTwo = document.getElementById("loading-two")
 var loadingText = document.getElementById("loading-text")
 
+var menuDiv = document.getElementById("menu")
+var presentationDiv = document.getElementById("presentation")
+var projectsDiv = document.getElementById("projects")
+var languageDiv = document.getElementById("language")
+
 function adjustDOM(timeout) {
   setTimeout(() => {
     loadingDiv.remove()
@@ -12,18 +17,18 @@ function adjustDOM(timeout) {
   }, timeout);
 }
 
-function loadingAnimation() {
+function loadingPageAnimation() {
   let adjustDOMTimeout = 0;
 
   if(typeof(Storage) !== "undefined" && (!localStorage.getItem("loaded") == "true" || localStorage.getItem("loaded") == null)) {
     loadingDiv.classList.remove("hide-div")
-    adjustDOMTimeout = 4250
+    adjustDOMTimeout = 3250
 
     setTimeout(() => {
       loadingOne.style.width = 0
       loadingTwo.style.width = 0
       loadingText.style.opacity = 0
-    }, 3000);
+    }, 2000);
     
     localStorage.setItem("loaded", "true") 
   }
@@ -31,4 +36,20 @@ function loadingAnimation() {
   adjustDOM(adjustDOMTimeout)
 }
 
-loadingAnimation();
+function loadComponentsAnimation() {  
+  setTimeout(() => {
+    menuDiv.style.transform = "translate(0,0)"
+
+    projectsDiv.style.transform = "rotate(-2deg) translate(0,0)"
+    projectsDiv.style.opacity = 1
+
+    presentationDiv.style.transform = "translate(0,0)"
+    presentationDiv.style.opacity = 1
+
+    languageDiv.style.left = "0px"
+  }, 2000);
+
+}
+
+loadComponentsAnimation();
+loadingPageAnimation();
