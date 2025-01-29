1c6631a3d96891b527cf45ec851335bbc5ecd86b
guilhermenakazato <guilherme.fernandes23903@gmail.com>
2025-01-28 22:07:22 -0400

    [FIX] Trying to fix project images container not scrolling with touch/mouse

--- scripts/projectInfo.js
+++ scripts/projectInfo.js
@@ -167,6 +167,8 @@ function grabImagesToScroll() {
   let pos = { top: 0, left: 0, x: 0, y: 0 };
 
   const mouseDownHandler = function (e) {
+    console.log("Hiya")
+
     projectImageDiv.style.cursor = "grabbing";
     projectImageDiv.style.userSprojectImageDivct = "none";
 
@@ -177,13 +179,16 @@ function grabImagesToScroll() {
       y: e.clientY,
     };
 
-    document.addEventListener("mousemove", mouseMoveHandler);
-    document.addEventListener("mouseup", mouseUpHandler);
+    document.addEventListener("pointermove", mouseMoveHandler);
+    document.addEventListener("pointerup", mouseUpHandler);
   };
 
   const mouseMoveHandler = function (e) {
     const dx = e.clientX - pos.x;
     const dy = e.clientY - pos.y;
+
+    console.log(dx)
+    console.log(dy)
 
     projectImageDiv.scrollTop = pos.top - dy;
     projectImageDiv.scrollLeft = pos.left - dx;
@@ -193,11 +198,11 @@ function grabImagesToScroll() {
     projectImageDiv.style.cursor = "grab";
     projectImageDiv.style.removeProperty("user-select");
 
-    document.removeEventListener("mousemove", mouseMoveHandler);
-    document.removeEventListener("mouseup", mouseUpHandler);
+    document.removeEventListener("pointermove", mouseMoveHandler);
+    document.removeEventListener("pointerup", mouseUpHandler);
   };
 
-  projectImageDiv.addEventListener("mousedown", mouseDownHandler);
+  projectImageDiv.addEventListener("pointerdown", mouseDownHandler);
 }
 
 function destroyProjectInfoDiv() {
